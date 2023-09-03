417b35050944d61c8b1ba8ab74c56ef8de4c3ce9
ExtremelyAmateurWebDeveloper <lemppugames@gmail.com>
2023-09-03 21:10:55 +0300

    yeah we defo using javascript

---
+++ MusicDisplay.js
@@ -0,0 +1,49 @@
+// JavaScript source code
+
+console.log('Test')
+let song = [{
+    name: 'Kys Vol.1',
+    artist: 'Thrashich',
+    desc: 'disgusting',
+    price: 'Free',
+    image: './media/AlbumCovers/KysVol1.png',
+    link: './index.html',
+},
+{
+    name: undefined,
+    artist: undefined,
+    desc: undefined,
+    price: undefined,
+    image: undefined,
+    link: undefined
+},
+{
+    name: undefined,
+    artist: undefined,
+    desc: undefined,
+    price: undefined,
+    image: undefined,
+    link: undefined
+},
+{
+    name: undefined,
+    artist: undefined,
+    desc: undefined,
+    price: undefined,
+    image: undefined,
+    link: undefined
+},
+{
+    name: undefined,
+    artist: undefined,
+    desc: undefined,
+    price: undefined,
+    image: undefined,
+    link: undefined
+}];
+
+let test = 'test';
+
+let currentpage = 0;
+
+currentpage = localStorage.getItem('pagenum')
