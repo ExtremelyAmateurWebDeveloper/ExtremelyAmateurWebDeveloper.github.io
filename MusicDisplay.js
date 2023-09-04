85f82f107fec9c125bbf86516398213a30a185c2
ExtremelyAmateurWebDeveloper <lemppugames@gmail.com>
2023-09-04 14:04:43 +0300

    Table to Unordered list

--- MusicDisplay.js
+++ MusicDisplay.js
@@ -7,15 +7,15 @@ let song = [{
     desc: 'disgusting',
     price: 'Free',
     image: './media/AlbumCovers/KysVol1.png',
-    link: './index.html',
+    link: 'https://drive.google.com/uc?id=1ssgZwXqiFs885RFlvJ4fBU78aS9ZJNdC&export=download',
 },
 {
-    name: undefined,
-    artist: undefined,
-    desc: undefined,
-    price: undefined,
-    image: undefined,
-    link: undefined
+    name: 'REMIXES EP. 1',
+    artist: 'Thrashich',
+    desc: 'containing remixes of artists such as "Radiohead", "Hatari", "Sydän, Sydän" and "D1 Dani"',
+    price: 'Free',
+    image: './media/remixes1.png',
+    link: 'https://drive.google.com/uc?id=1ssgZwXqiFs885RFlvJ4fBU78aS9ZJNdC&export=download'
 },
 {
     name: undefined,
