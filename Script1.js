cf1e5b8650fc9177422b597ed25e592f994e8cb4
ExtremelyAmateurWebDeveloper <lemailbusiness171@gmail.com>
2023-12-09 02:08:39 +0200

    Updated stylesheet to work with more screens and began work on THE FLOW

---
+++ Script1.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+let indexes = ["", "", "", "", "", "", "", ""]
+
+
+
+function ChatUpload(input) {
+    for (let i = 0; (indexes[i]) = ""; i++) {
+        if (i = 7) {
+            if (indexes[7] != "") {
+
+                indexes = ["", "", "", "", "", "", "", ""]
+            } else {
+
+
+            }
+
+
+        } else {
+            if (indexes[i] = "") {
+                console.log("emptyslot" + i)
+                indexes[i] = input
+                break
+            }
+
+        }
+
+
+
+  }
+
+
+}
